c70eb8b85185d65adad9dcaa987dd31ed7472c79
fralorange <78620105+fralorange@users.noreply.github.com>
2024-01-14 20:22:27 +0300

    Initial commit

---
+++ src/FreelanceBotBase.Bot/Commands/Const/CommandsDictionary.cs
@@ -0,0 +1,14 @@
+﻿using FreelanceBotBase.Bot.Commands.Greeting;
+using FreelanceBotBase.Bot.Commands.Interface;
+using Telegram.Bot;
+
+namespace FreelanceBotBase.Bot.Commands.Const
+{
+    public static class CommandsDictionary
+    {
+        public readonly static Dictionary<string, Func<ITelegramBotClient, ICommand>> All = new()
+        {
+            { "/hi", botClient => new GreetingCommand(botClient) },
+        };
+    }
+}
---
+++ src/FreelanceBotBase.Bot/Program.cs
@@ -0,0 +1,35 @@
+﻿using FreelanceBotBase.Infrastructure.Settings;
+using FreelanceBotBase.Infrastructure.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using FreelanceBotBase.Bot.Handlers.Update;
+using FreelanceBotBase.Bot.Services.Receiver;
+using FreelanceBotBase.Bot.Services.Polling;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.Configure<BotConfiguration>(
+            context.Configuration.GetSection(BotConfiguration.Configuration));
+
+        services.Configure<ReceiverOptions>(
+            context.Configuration.GetSection(nameof(ReceiverOptions)));
+
+
+        services.AddHttpClient("telegram_bot_client")
+            .AddTypedClient<ITelegramBotClient>((httpClient, sp) =>
+            {
+                BotConfiguration? botConfig = sp.GetConfiguration<BotConfiguration>();
+                TelegramBotClientOptions options = new(botConfig.BotToken);
+                return new TelegramBotClient(options, httpClient);
+            });
+
+        services.AddScoped<UpdateHandler>();
+        services.AddScoped<ReceiverService>();
+        services.AddHostedService<PollingService>();
+    })
+    .Build();
+
+await host.RunAsync();
